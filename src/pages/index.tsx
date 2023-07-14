9dcdb26b2737d6c7b29f86b12953c2291b1c4c42
Tommi Enenkel <tommi.enenkel@gmail.com>
2023-07-15 00:32:46 +0200

    styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs">
             Go down the Rabbithole
           </Link>
         </div>
@@ -33,9 +33,11 @@ export default function Home(): JSX.Element {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
+  /*
+<main>
+        <HomepageFeatures />
+      </main>
+  */
 }
